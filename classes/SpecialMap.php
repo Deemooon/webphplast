06f335edac0000f02b99283e5fdc272d3ca11b4b
Deemooon <tenno1503@gmail.com>
2020-06-03 23:36:22 +0600

    5 commit

--- classes/SpecialMap.php
+++ classes/SpecialMap.php
@@ -1,5 +1,8 @@
 <?php
 
 class SpecialMap extends BaseMap {
-
+    public function arrSpecials(){
+        $res = $this->db->query("SELECT special_id AS id, name AS value FROM special");
+        return $res->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
