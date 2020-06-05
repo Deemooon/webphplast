ea3c4cf93078c586fcb54e1598d395119e07f14b
Deemooon <tenno1503@gmail.com>
2020-06-05 15:38:25 +0600

    6 commit

--- classes/LessonPlan.php
+++ classes/LessonPlan.php
@@ -1,14 +1,18 @@
 <?php
 
-class LessonPlan extends Table {
-
-    public $lesson_plan_id = 0;
-    public $gruppa_id = 0;
-    public $subject_id = 0;
-    public $user_id = 0;
-
+class LessonPlan extends Table
+{
     public function validate()
     {
-        return false;
+        try {
+            if (!empty($this->gruppa_id) && !empty($this->subject_id) && !empty($this->user_id)) {
+                return true;
+            } else {
+                throw new Exception('Не переданны все параметры');
+            }
+        } catch (Exception $ex) {
+            return false;
+        }
+
     }
 }
